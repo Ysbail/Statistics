5578e2baf59f5f796e616ab09b6dba7f7e885f87
Ysbail <rudowlosa@gmail.com>
2020-05-16 19:53:34 +0200

    Delete ecdf.py

--- ecdf.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def ecdf(data):
-    """Compute ECDF for a one-dimensional array of measurements."""
-    n = len(data)
-    x = np.sort(data)
-    y = np.arange(1, n+1) / n
-
-    return x, y
